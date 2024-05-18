200171cd793f3945d9f1c82851301e37ab8507fc
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-05-18 17:40:21 +0530

    Alternate command + keyboardInterrupt exception ahndle

--- build/lib/psgr/main.py
+++ build/lib/psgr/main.py
@@ -1,4 +1,5 @@
 import json
+import random
 import sys
 import time
 import base64
@@ -9,7 +10,6 @@ import json
 import rsa
 from InquirerPy import inquirer
 import signal
-
 
 
 def main():
@@ -87,6 +87,10 @@ def main():
         elif command == "keys":
             (pub, priv) = fetchKeys()
             print(pub, priv)
+        elif command == "genkeys":
+            generateKeys()
+        elif command == "genpass":
+            passwordGen()
 
         return
 
@@ -101,7 +105,7 @@ def main():
                 return
             
         if not password:        
-            password = getpass.getpass(f"Enter Password for {accountName}: ").encode("utf-8")
+            password = getpass.getpass(f"Enter Password for {accountName}: ", stream=sys.stderr).encode("utf-8")
         else:
             password = password.encode("utf-8")
 
@@ -113,6 +117,7 @@ def main():
         except Exception as e:
             print("An error occurred while writing to the file:", e)
         # print(accounts)
+        print("Password saved successfully.")
         return
 
     def show(name=""):
@@ -161,20 +166,28 @@ def main():
                 print(table)
                 print(" ")
 
+            tableLines = table.count("\n") + 2
+
         def ignore_ctrl_c(signum, frame):
-            print("Ctrl+C is disabled.", end="\r")
+            print("                             ", end="\r")
+            for i in range(tableLines):
+                print("\033[F", end="")  # Move cursor up one line
+                print("\033[K", end="")
+            sys.exit(0)
+        
         original_handler = signal.getsignal(signal.SIGINT)
         signal.signal(signal.SIGINT, ignore_ctrl_c)
-        for i in range(5, 0, -1):
+        for i in range(7, 0, -1):
             print(f"clearing screen in {i} seconds.", end="\r")
             time.sleep(1)
+        print("                             ", end="\r")
         signal.signal(signal.SIGINT, original_handler)
-        if os.name == 'nt':
-            _ = os.system('cls')
-        # For UNIX-like systems (Linux, macOS)
-        else:
-            _ = os.system('clear')
-        return
+        
+        for i in range(tableLines):
+            print("\033[F", end="")  # Move cursor up one line
+            print("\033[K", end="")
+        
+        sys.exit(0)
 
     def help():
         help_message = """
@@ -187,20 +200,27 @@ def main():
         add          Add a new account
         show         Show account information
         refresh      Refresh account information
+        genpass      Create and generate a strong password of the length of your choice
+        keys         View your current RSA encryption keys protecting your passwords
+        genkeys      Generate new keys. This will also be done automatically everytime you run "passman show".
         help         Show this help message
 
     Examples:
+        passman add                        
         passman add Google                # Add a new account named "Google"
         passman add Facebook password     # Add a new account named "Facebook" with password "password"
+        passman show                        # Promted to a list to choose your account from
         passman show all                       # Show all accounts
-        passman show Google               # Show account information for "Google"
-        passman refresh                     # Refresh account information
+        passman show Google               # Show account password for "Google"
+        passman refresh                     # Refresh keys
 
-    For more information, visit: https://github.com/nareshkarthigeyan/passwordManager
+    For more information and source code, visit: https://github.com/nareshkarthigeyan/passwordManager
 
     Note:
-        - Passwords are encrypted at all times.
-        - Passwords will disappear from the screen after 5 seconds.
+        - Passwords are encrypted using RSA encryption.
+        - Passwords will disappear from the screen after 7 seconds.
+
+    Made with Love by K V Naresh Karthigeyan
     """
         print(help_message)
         return
@@ -229,11 +249,48 @@ def main():
                 with open("accountinfo.json", "w") as f:
                     json.dump(accounts, f)            
         return
+    
+    def genpass(length):
+        digits = [x for x in "0123456789"]
+        lowercase = [x for x in "abcdefghijklmnopqrstuvwxyz"]
+        uppercase = [x for x in "abcdefghijklmnopqrstuvwxyz".upper()]
+        special = [x for x in "!@#$%^&*()_+~|}{:'?><" +'"']
+        combinedList = digits + lowercase + uppercase + special
 
+        password = "" + random.choice(uppercase) + random.choice(lowercase) + random.choice(special)
+        for i in range (length - 3):
+            password += random.choice(combinedList)
+
+        return password
+    
+    def passwordGen():
+        length = inquirer.select(
+                    message = "Choose Length of Password to be generated:",
+                    choices = [12, 16, 24]
+                ).execute()
+        password = genpass(length=length)
+        print("\t", password)
+        savePassword = inquirer.select(
+                    message = "Do you want to save this password for an account?",
+                    choices = ["yes", "no"]
+                ).execute()
+        if savePassword == "yes":
+            chooseIt = ["Create a New Account"]
+            for k, v in accounts.items():
+                chooseIt.append(k)
+            accountName = inquirer.select(
+                message = "Choose the account:",
+                choices = chooseIt
+            ).execute()
+
+            if accountName == "Create a New Account":
+                add(password=password)
+            else:
+                add(accountName=accountName, password=password)
 
     n = len(sys.argv)
 
-    arguments = ("help", "add", "show", "remove", "keys", "refresh")
+    arguments = ("help", "-h", "add", "-a", "show", '-s', "remove", "-rm", "keys", "refresh", "-r" "genkeys", "genpass", "-p")
 
     if os.path.isfile("accountinfo.json"):
         with open("accountinfo.json", "r") as f:
@@ -256,4 +313,4 @@ def main():
         assign_task(sys.argv[1], sys.argv[2], sys.argv[3]) 
 
 
-main()
+#main()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='psgr',
-    version='0.1.3',
+    version='0.2.1',
     description='A password manager application',
     author='Naresh Karthigeyan',
     author_email='nareskarthigeyan.2005@gmail.com',
